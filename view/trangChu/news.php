424983c010f206439db78f77ef15a5a4bbc95741
HasterIT <dp73287@gmail.com>
2024-12-02 12:07:59 +0700

    ads

--- view/trangChu/news.php
+++ view/trangChu/news.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <style>
         * {
@@ -33,6 +34,7 @@
             text-decoration: none;
             color: black;
         }
+
         .title {
             text-align: center;
             font-size: 36px;
@@ -40,6 +42,7 @@
             line-height: 54px;
             margin-bottom: 20px;
         }
+
         .main {
             width: 100%;
             background-color: #F5F5F7;
@@ -47,71 +50,80 @@
             margin-top: 10px;
             padding-top: 10px;
         }
-        .news-showcase{
+
+        .news-showcase {
             width: 100%;
             flex-wrap: wrap;
         }
-        .news-grid{
+
+        .news-grid {
             width: 1200px;
             justify-content: center;
             gap: 20px;
             display: grid;
-            grid-template-columns: 1fr 1fr ;
+            grid-template-columns: 1fr 1fr;
         }
-        .box{
+
+        .box {
             width: 100%;
             display: flex;
             justify-content: center;
             align-items: center;
             flex-wrap: wrap;
-            
+
             border-bottom: 3px solid #ddd;
         }
-        .news{
+
+        .news {
             width: 100%;
             display: flex;
             justify-content: center;
             margin: 5px;
         }
-        .news img{
+
+        .news img {
             border-radius: 10px;
-        } 
-        .news .nd .product_name{
+        }
+
+        .news .nd .product_name {
             font-size: 30px;
-            
+
         }
     </style>
 </head>
+
 <body>
-    <?php include "/laragon/www/DA1/components/header.php"; ?>
-<div class="container">
+    <?php include "components/header.php"; ?>
+    <div class="container">
         <div class="link">
             <a href="?act=home">Trang chủ ›</a>
-            <p>Tin Tức  </p>
+            <p>Tin Tức </p>
         </div>
-    <div class="main">
-        <div class="title">Tin Tức </div>
-        <div class="news-showcase">
-            <div class="news-grid">
-            <?php foreach ($all_news as $key) { ?>
-                            <div class="box1 box active">
-                                <!-- img o trong db : 	/upload/ip16.jpeg -->
-                                <div class="news">
-                                    <a href="?act=chiTietNew&id=<?= $key->new_id ?>">
-                                        <img src="<?= $key->thumbnail ?>" width="240px" height="240px" alt="">
-                                        
-                                    </a>
-                                    <div class="nd">
+        <div class="main">
+            <div class="title">Tin Tức </div>
+            <div class="news-showcase">
+                <div class="news-grid">
+                    <?php foreach ($all_news as $key) { ?>
+                        <div class="box1 box active">
+                            <!-- img o trong db : 	/upload/ip16.jpeg -->
+                            <div class="news">
+                                <a href="?act=chiTietNew&id=<?= $key->new_id ?>">
+                                    <img src="<?= $key->thumbnail ?>" width="240px" height="240px" alt="">
+
+                                </a>
+                                <div class="nd">
                                     <div class="product_name"><?= $key->title ?></div>
-                                    <div class="product_price"><i class="fa-solid fa-calendar-days"></i>  <?= $key->create_at ?></div>
-                                    </div>
+                                    <div class="product_price"><i class="fa-solid fa-calendar-days"></i>
+                                        <?= $key->create_at ?></div>
                                 </div>
                             </div>
-                        <?php } ?>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<?php include "/laragon/www/DA1/components/footer.php"; ?>
+    <?php include "components/footer.php"; ?>
 </body>
+
 </html>
